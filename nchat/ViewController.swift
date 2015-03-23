c21b86c7523844177662bb6b18daa1a09d69ccf7
Evan Carmi <evan@ecarmi.org>
2015-03-23 18:51:31 -0300

    Initial Commit

---
+++ nchat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nchat
+//
+//  Created by Evan Carmi on 3/23/15.
+//  Copyright (c) 2015 Evan Carmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
